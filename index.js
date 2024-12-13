946ee5eda6100e1d6a238d564839d29851a9fb20
khushithool <khushithool0@gmail.com>
2024-12-13 12:01:01 +0530

    this is file

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 import express from "express"
 let aap = express()
 let port = 3050
-aap.listen(port =>{
-    console.log("welcom to server")
+aap.get("/", (req , res) => {
+   
+    
+
+    res.json(
+        {
+            "message": "this is file",
+            "data": 2314,
+            "name": "khushi thool",
+            "array": [1,2,3,4,5,6,],
+            "age":21,
+           "email":"khushithool0@gmail.com",
+            "isopen": true
+        }
+    )
+})
+aap.listen(port, () => {
+    console.log(`server is running on port :${port} | http://localhost:${port}`)
 })
